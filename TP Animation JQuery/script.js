f0c8e7baa0ec7bccc332aabebccbe9fb9fdfb64c
Florent <florent.pouzol@etu.univ-grenoble-alpes.fr>
2019-12-18 17:49:34 +0100

    Modifs tp dynamique

--- TP Animation JQuery/script.js
+++ TP Animation JQuery/script.js
@@ -8,10 +8,25 @@ $('#add').click(function () {
         currentProgress = currentProgress + 10;
         $('#progress-bar').css('width', currentProgress+'%');
     }
+    $('#progresstext').val(currentProgress);
 });
 
 
 $('#init').click(function () {
     currentProgress = 0;
     $('#progress-bar').css('width', '0%');
+    $('#progresstext').val(currentProgress);
+});
+
+$('#progresstext').change(function () {
+    if (this.value < 0) {
+        currentProgress = 0;
+    } else if (this.value > 100) {
+        currentProgress = 100;
+    } else {
+        currentProgress = this.value;
+    }
+
+    $('#progresstext').val(currentProgress);
+    $('#progress-bar').css('width', currentProgress+'%');
 });
